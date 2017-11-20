a32fa5c0d9675650d74f287942f5e051a8620b7d
Sun Haohao <viringbells@q..com>
2017-11-20 18:03:50 +0800

    fix test

--- test/index.js
+++ test/index.js
@@ -24,12 +24,12 @@ describe('common use', () => {
         descriptor.should.have.property('files');
         descriptor.files.should.be.an.instanceOf(Object);
         const keys = Object.keys(descriptor.files);
-        keys.length.should.be.exactly(4);
-        for (const name of ['file.txt', 'js_file.js', 'somesocket', 'sub_dir']) {
+        keys.length.should.be.exactly(3);
+        for (const name of ['file.txt', 'js_file.js', 'sub_dir']) {
             keys.includes(name).should.be.ok;
             descriptor.files[name].should.be.an.instanceOf(Descriptor);
         }
-        for (const file_name of ['file.txt', 'js_file.js', 'somesocket']) {
+        for (const file_name of ['file.txt', 'js_file.js']) {
             descriptor.files[file_name].stats.isFile().should.be.ok;
         }
         descriptor.files.sub_dir.stats.isDirectory().should.be.ok;
@@ -41,7 +41,6 @@ describe('common use', () => {
         tree.should.containDeep({
             'file.txt': misc.path.absolute('directory/file.txt'),
             'js_file.js': misc.path.absolute('directory/js_file.js'),
-            'somesocket': misc.path.absolute('directory/somesocket'),
             'sub_dir': {
                 'sub_2_dir': {
                     'a.txt': misc.path.absolute('directory/sub_dir/sub_2_dir/a.txt'),
